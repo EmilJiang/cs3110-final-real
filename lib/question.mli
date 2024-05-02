9d4c2310ba6fe56647f23883904004ea8765884d
Cindy Li <cl2674@cornell.edu>
2024-05-01 21:36:05 -0400

    Added question.mli functions
    - Added more functions

--- lib/question.mli
+++ lib/question.mli
@@ -1,2 +1,5 @@
 val output : string -> Pytypes.pyobject -> Pytypes.pyobject * string
 val count_characters : string -> int
+val add_to_array : string array -> string -> string array
+val round_up_division : int -> int -> int
+val is_printable : char -> bool
