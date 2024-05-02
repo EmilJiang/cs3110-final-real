9a643b7278b89c2c176f44e9461ca2a3e736b004
Cindy Li <cl2674@cornell.edu>
2024-05-01 21:35:22 -0400

    Added tests
    - Added is printable tests
    - Added round up tests

--- test/test_final_project_test.ml
+++ test/test_final_project_test.ml
@@ -15,7 +15,7 @@ let tests =
            let counted_characters = count_characters text in
            let equivalent_counters = 0 in
            assert_equal counted_characters equivalent_counters );
-         ( "output - Empty text returns 0" >:: fun _ ->
+         ( "output - short text" >:: fun _ ->
            let text = "Hi" in
            let output1 = output text in
            let py_empty_list = Py.List.of_list [] in
@@ -23,7 +23,7 @@ let tests =
            let length = String.length str in
            let boo = length > 0 in
            assert_equal boo true );
-         ( "output - Empty text returns 0" >:: fun _ ->
+         ( "output - Long text" >:: fun _ ->
            let text =
              "The cozy seating arrangements, with their plush cushions and \
               warm lighting, beckon visitors to settle in and unwind. It's a \
@@ -37,7 +37,7 @@ let tests =
            let length = String.length str in
            let boo = length > 0 in
            assert_equal boo true );
-         ( "output - Empty text returns 0" >:: fun _ ->
+         ( "output - empty text" >:: fun _ ->
            let text = "0" in
            let output1 = output text in
            let py_empty_list = Py.List.of_list [] in
@@ -85,6 +85,43 @@ let tests =
                 (Course.edit_course_description Course.empty_course "course1")
                 (Course.edit_course_description Course.empty_course "course2"))
          );
+         ( "add to array - add one elem" >:: fun _ ->
+           let arr = add_to_array (Array.make 0 "") "Hi" in
+           let length = Array.length arr in
+           let boo = length > 0 in
+           assert_equal boo true );
+         ( "add to array - add two elem" >:: fun _ ->
+           let arr = add_to_array (Array.make 0 "") "Hi" in
+           let arr2 = add_to_array arr "Hi" in
+           let length = Array.length arr2 in
+           let boo = length = 2 in
+           assert_equal boo true );
+         ( "round up division - 1/2" >:: fun _ ->
+           let num = 3 in
+           let denom = 6 in
+           let sol = round_up_division num denom in
+           let ans = 1 in
+           assert_equal sol ans );
+         ( "round up division - 0 num" >:: fun _ ->
+           let num = 0 in
+           let denom = 6 in
+           let sol = round_up_division num denom in
+           let ans = 0 in
+           assert_equal sol ans );
+         ( "round up division - rand num" >:: fun _ ->
+           let num = 18 in
+           let denom = 7 in
+           let sol = round_up_division num denom in
+           let ans = 3 in
+           assert_equal sol ans );
+         ( "is printable - random letter" >:: fun _ ->
+           let char = 'c' in
+           let boo = is_printable char in
+           assert_equal boo true );
+         ( "is printable - random character" >:: fun _ ->
+           let char = ')' in
+           let boo = is_printable char in
+           assert_equal boo true );
        ]
 
 let _ = run_test_tt_main tests
