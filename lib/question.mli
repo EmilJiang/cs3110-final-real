ec9b16d2b0fd34108f3e79f079cd639194c80229
Cindy Li <cl2674@cornell.edu>
2024-05-04 17:48:46 -0400

    Added more to question.mli
    - Exposed more functions
    - Tested

--- lib/question.mli
+++ lib/question.mli
@@ -3,3 +3,7 @@ val count_characters : string -> int
 val add_to_array : string array -> string -> string array
 val round_up_division : int -> int -> int
 val is_printable : char -> bool
+val remove_newlines_and_spaces : string -> string
+val sub_array : 'a array -> int -> int -> 'a array
+val calculate_total_y_offset : string array -> int -> int
+val find_starting_index : string array -> int -> int -> int
