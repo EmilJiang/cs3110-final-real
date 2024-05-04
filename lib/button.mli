031f71a7bceab6e52e2d15a1cd641b60a7067aba
Cindy Li <cl2674@cornell.edu>
2024-05-04 17:49:56 -0400

    Added documentation
    - Added comments to button.mli

--- lib/button.mli
+++ lib/button.mli
@@ -1,3 +1,5 @@
+(**Button type t represents a clickable button
+    *)
 type t = {
   x : int;
   y : int;
@@ -5,11 +7,20 @@ type t = {
   height : int;
 }
 
+(**[default button] returns a default button*)
 val default_button : t
+(**[new_button] returns a new button based off size parameters*)
 val new_button : int -> int -> int -> int -> t
+(**[button_x] returns x value of button*)
 val button_x : t -> int
+(**[button_y] returns y value of button*)
 val button_y : t -> int
+(**[button_width] returns width value of button*)
 val button_width : t -> int
+(**[button_height] returns height value of button*)
 val button_height : t -> int
+(**[compare_button] compares two buttons and returns true if they are the same*)
 val compare_button : t -> t -> bool
+
+(**[button_to_string] converts a button to string*)
 val button_to_string : t -> string
