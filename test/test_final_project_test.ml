77f6bdfccf071a009acb8439d2a3f82abdee5eb9
Cindy Li <cl2674@cornell.edu>
2024-05-04 17:36:44 -0400

    Added tests
    - Tested finding starting index
    - Tested calc total y offset

--- test/test_final_project_test.ml
+++ test/test_final_project_test.ml
@@ -96,6 +96,151 @@ let tests =
            let length = Array.length arr2 in
            let boo = length = 2 in
            assert_equal boo true );
+         ( "remove_newlines_and_spaces - no newlines or spaces" >:: fun _ ->
+           let str = "string" in
+           let str2 = remove_newlines_and_spaces str in
+           assert_equal str str2 );
+         ( "remove_newlines_and_spaces - w/ new lines" >:: fun _ ->
+           let str = "string\nhi" in
+           let str2 = remove_newlines_and_spaces str in
+           let str3 = "stringhi" in
+           assert_equal str2 str3 );
+         ( "remove_newlines_and_spaces - w/ spaces" >:: fun _ ->
+           let str = "string hi" in
+           let str2 = remove_newlines_and_spaces str in
+           let str3 = "stringhi" in
+           assert_equal str2 str3 );
+         ( "subarr - empty arr" >:: fun _ ->
+           let arr = Array.make 0 "" in
+           let p1 = 0 in
+           let p2 = 0 in
+           let other_arr = sub_array arr p1 p2 in
+           assert_equal arr other_arr );
+         ( "subarr - single point arr" >:: fun _ ->
+           let arr = add_to_array (Array.make 0 "") "Hi" in
+           let p1 = 0 in
+           let p2 = 0 in
+           let other_arr = sub_array arr p1 p2 in
+           assert_equal arr other_arr );
+         ( "subarr - single point arr" >:: fun _ ->
+           let arr = add_to_array (Array.make 0 "") "Hi" in
+           let arr2 = Array.make 0 "" in
+           let p1 = -1 in
+           let p2 = -1 in
+           let other_arr = sub_array arr p1 p2 in
+           assert_equal arr2 other_arr );
+         ( "subarr - double point arr" >:: fun _ ->
+           let arr =
+             add_to_array (add_to_array (Array.make 0 "") "Hi") "hello"
+           in
+           let arr2 = add_to_array (Array.make 0 "") "Hi" in
+           let p1 = 0 in
+           let p2 = 0 in
+           let other_arr = sub_array arr p1 p2 in
+           assert_equal arr2 other_arr );
+         ( "subarr - double point arr" >:: fun _ ->
+           let arr =
+             add_to_array (add_to_array (Array.make 0 "") "Hi") "hello"
+           in
+           let p1 = 0 in
+           let p2 = 1 in
+           let other_arr = sub_array arr p1 p2 in
+           assert_equal arr other_arr );
+         ( "calculate_total_y_offset - empty arr" >:: fun _ ->
+           let arr = Array.make 0 "" in
+           let p = 0 in
+           let vali = 0 in
+           let x = calculate_total_y_offset arr p in
+           assert_equal vali x );
+         ( "calculate_total_y_offset - 1 arr" >:: fun _ ->
+           let arr = add_to_array (Array.make 0 "") "Hi" in
+           let p = 0 in
+           let vali = 2 in
+           let x = calculate_total_y_offset arr p in
+           assert_equal vali x );
+         ( "calculate_total_y_offset - 2 arr" >:: fun _ ->
+           let arr =
+             add_to_array (add_to_array (Array.make 0 "") "Hi") "hello"
+           in
+           let p = 0 in
+           let vali = 4 in
+           let x = calculate_total_y_offset arr p in
+           assert_equal vali x );
+         ( "calculate_total_y_offset - 2 arr" >:: fun _ ->
+           let arr =
+             add_to_array (add_to_array (Array.make 0 "") "Hi") "hello"
+           in
+           let p = 1 in
+           let vali = 6 in
+           let x = calculate_total_y_offset arr p in
+           assert_equal vali x );
+         ( "calculate_total_y_offset - 3 arr" >:: fun _ ->
+           let arr =
+             add_to_array
+               (add_to_array (add_to_array (Array.make 0 "") "Hi") "hello")
+               "hi"
+           in
+           let p = 0 in
+           let vali = 6 in
+           let x = calculate_total_y_offset arr p in
+           assert_equal vali x );
+         ( "calculate_total_y_offset - 3 arr" >:: fun _ ->
+           let arr =
+             add_to_array
+               (add_to_array (add_to_array (Array.make 0 "") "Hi") "hello")
+               "hi"
+           in
+           let p = 1 in
+           let vali = 9 in
+           let x = calculate_total_y_offset arr p in
+           assert_equal vali x );
+         ( "find_starting_index - empty arr" >:: fun _ ->
+           let arr = add_to_array (Array.make 0 "") "Hi" in
+           let p = 0 in
+           let t = 1 in
+           let vali = 1 in
+           let x = find_starting_index arr p t in
+           assert_equal vali x );
+         ( "find_starting_index - 1 arr" >:: fun _ ->
+           let arr = Array.make 0 "" in
+           let p = 0 in
+           let t = 1 in
+           let vali = 0 in
+           let x = find_starting_index arr p t in
+           assert_equal vali x );
+         ( "find_starting_index - 3 arr" >:: fun _ ->
+           let arr =
+             add_to_array
+               (add_to_array (add_to_array (Array.make 0 "") "Hi") "hello")
+               "hi"
+           in
+           let p = 0 in
+           let t = 3 in
+           let vali = 3 in
+           let x = find_starting_index arr p t in
+           assert_equal vali x );
+         ( "find_starting_index - 3 arr" >:: fun _ ->
+           let arr =
+             add_to_array
+               (add_to_array (add_to_array (Array.make 0 "") "Hi") "hello")
+               "hi"
+           in
+           let p = 2 in
+           let t = 3 in
+           let vali = 3 in
+           let x = find_starting_index arr p t in
+           assert_equal vali x );
+         ( "find_starting_index - 3 arr" >:: fun _ ->
+           let arr =
+             add_to_array
+               (add_to_array (add_to_array (Array.make 0 "") "Hi") "hello")
+               "hi"
+           in
+           let p = 1 in
+           let t = 1 in
+           let vali = 3 in
+           let x = find_starting_index arr p t in
+           assert_equal vali x );
          ( "round up division - 1/2" >:: fun _ ->
            let num = 3 in
            let denom = 6 in
