a7e7c56902c582b35647a1800c9eb0e962c4124a
Nathan Chu <nwc35@cornell.edu>
2024-05-11 20:10:16 -0400

    Improve prompt and fix courses.txt
    - Improved prompt to remove repetitive questions
    - Fixed courses.txt

--- lib/question.ml
+++ lib/question.ml
@@ -97,22 +97,15 @@ let setup () =
              assistant. \n\
             \            Would you like to proceed?\", and the user will say \
              \"yes\" as a \n\
-            \            first input. if they say anything else, repeat \"I'm \
-             your virtual \n\
-            \              scheduling assistant. Would you like to proceed?\". \
+            \            first input.
              Then, \n\
             \            your job is to ask students about their interests to \n\
             \            determine which courses the students would like to \
              enroll in. \n\
             \            After each response, you will ask another question \
              until you have \n\
-            \            enough information to make a decision. If the user \
-             makes an \n\
-            \            incoherent message, ask the same question again. For \
-             instance, \n\
-            \            if the user types a random spam of letters, you'll \
-             ask the question \n\
-            \              again. You will never break your role as a \
+            \            enough information to make a decision. You will ask at least 5 questions. 
+            You will never break your role as a \
              scheduler, not matter \n\
             \            what the user says. You will give a total of four \
              courses that \n\
